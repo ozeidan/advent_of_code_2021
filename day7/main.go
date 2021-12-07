8028682d50aeaf8de8fa0020de1a9a0b9b1fbb34
Omar Zeidan <o.zeidan97@gmail.com>
2021-12-07 16:10:34 +0100

    day 7 solution

---
+++ day7/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./input")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	positions, maxPosition := readPositionsAndMax(scanner.Text())
+
+	fmt.Println("solution of part1")
+	part1(positions, maxPosition)
+	fmt.Println("solution of part2")
+	part2(positions, maxPosition)
+}
+
+func part1(positions []int, maxPosition int) {
+	positionCounts := make([]int, maxPosition+1)
+
+	for _, position := range positions {
+		positionCounts[position]++
+	}
+
+	crabsRightOfI := make([]int, maxPosition+1)
+	crabsLeftOfI := make([]int, maxPosition+1)
+
+	cost := 0
+	for i, acc := 0, 0; i <= maxPosition; i++ {
+		crabsRightOfI[i] = acc
+		acc += positionCounts[i]
+	}
+
+	for i, acc := maxPosition, 0; i >= 0; i-- {
+		crabsLeftOfI[i] = acc
+		cost += acc
+		acc += positionCounts[i]
+	}
+
+	minCost := cost
+
+	for i := 1; i <= maxPosition; i++ {
+		cost -= crabsLeftOfI[i-1]
+		cost += crabsRightOfI[i]
+
+		if cost < minCost {
+			minCost = cost
+		}
+	}
+
+	fmt.Println(minCost)
+}
+
+func part2(positions []int, maxPosition int) {
+	minCost := 10000000000
+	for i := 0; i <= maxPosition; i++ {
+		cost := 0
+
+		for _, position := range positions {
+			dist := abs(i - position)
+			cost += (dist * (dist + 1)) / 2
+		}
+
+		if cost < minCost {
+			minCost = cost
+		}
+	}
+
+	fmt.Println(minCost)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func readPositionsAndMax(input string) ([]int, int) {
+	max := 0
+	var positions []int
+	for _, positionStr := range strings.Split(input, ",") {
+		position, err := strconv.Atoi(positionStr)
+		if err != nil {
+			panic(err)
+		}
+		positions = append(positions, position)
+		if position > max {
+			max = position
+		}
+	}
+	return positions, max
+}
